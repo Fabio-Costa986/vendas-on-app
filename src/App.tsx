6692a511394d7d69b6bb5c011f2122cd7b5fb977
Fabio Costa <fscosta114@gmail.com>
2023-09-07 10:43:41 -0300

    inicio do projeto

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { SafeAreaView, Text} from "react-native";
+
+const App = () => {
+
+    return (
+        <SafeAreaView>
+            <Text>Testando</Text>
+        </SafeAreaView>
+    )
+
+
+};
+
+export default App;
